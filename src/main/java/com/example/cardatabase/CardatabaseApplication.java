4056f1e984622695dc6b5a5e0845fd14c1803137
Mohammad Kilane <mohmad.kilane.94@gmail.com>
2018-12-17 20:22:48 +0200

    First Commite and push

--- src/main/java/com/example/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/example/cardatabase/CardatabaseApplication.java
@@ -2,12 +2,19 @@ package com.example.cardatabase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class CardatabaseApplication {
+	  private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
+		//rain
+		//fffffffff
+	    logger.info("Hello Spring Boot");
+
 	}
 
 }
